f46f8ac925760015ee9931b767906c068d5460c9
Rodrigo Kalil <rkbuarque@gmail.com>
2023-11-16 18:36:50 -0300

    feat: adds auth0 auth

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,8 +1,13 @@
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { AppProps } from 'next/app';
 import './globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {... pageProps}/>
+  return (
+    <UserProvider>
+    <Component {... pageProps}/>
+    </UserProvider>
+  )
 }
 
 export default MyApp
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,6 +1,44 @@
+import { getAccessToken, getSession } from "@auth0/nextjs-auth0"
+import { useUser } from "@auth0/nextjs-auth0/client"
+import { GetServerSideProps } from "next"
 
 export default function Home() {
-  return (
-   <h1>Hello Next!</h1>
-  )
+  // const { user } = useUser()
+  // return (
+  //   <div>
+  //      <h1>Hello Next!</h1>
+
+  //      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+  //      <a href="/api/auth/login">Login</a>
+  //   </div>
+  // )
+  return null;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  // const token = await getAccessToken(req, res);
+
+  // console.log(token);
+
+  // return {
+  //   props: {},
+  // }
+  const session = await getSession(req, res);
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/login',
+        permanent: false,
+      }
+    }
+  } else {
+    return {
+      redirect: {
+        destination: '/app',
+        permanent: false,
+      }
+    }
+  }
 }
