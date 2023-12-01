c9cc18cee394b7d76842dcc7c56f4001109de119
Rodrigo Kalil <rkbuarque@gmail.com>
2023-12-01 10:11:41 -0300

    feat: adds on classroom microservice its functionalities

---
+++ classroom/src/http/graphql/resolvers/students.resolver.ts
@@ -0,0 +1,32 @@
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { EnrollmentsService } from '../../../services/enrollments.service';
+import { StudentsService } from '../../../services/students.service';
+import { AuthorizationGuard } from '../../../authorization/authorization.guard';
+import { CurrentUser, AuthUser } from '../../../authorization/current-user';
+import { Student } from '../models/student';
+
+@Resolver(() => Student)
+export class StudentsResolver {
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
+
+  @Query(() => Student)
+  @UseGuards(AuthorizationGuard)
+  me(@CurrentUser() user: AuthUser) {
+    return this.studentsService.getStudentByAuthUserId(user.sub);
+  }
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollments(@Parent() student: Student) {
+    return this.enrollmentsService.listEnrollmentsByStudent(student.id);
+  }
+}
--- classroom/src/http/http.module.ts
+++ classroom/src/http/http.module.ts
@@ -1,10 +1,15 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../database/database.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
-import { TestResolver } from './test.resolver';
-import { ApolloDriver } from '@nestjs/apollo';
+import { CoursesService } from '../services/courses.service';
+import { EnrollmentsService } from '../services/enrollments.service';
+import { StudentsService } from '../services/students.service';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
@@ -15,6 +20,16 @@ import { ApolloDriver } from '@nestjs/apollo';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    // Resolvers
+    CoursesResolver,
+    EnrollmentsResolver,
+    StudentsResolver,
+
+    // Services
+    CoursesService,
+    EnrollmentsService,
+    StudentsService,
+  ],
 })
 export class HttpModule {}
