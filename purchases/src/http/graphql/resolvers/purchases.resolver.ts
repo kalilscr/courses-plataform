051448dcb38d8571cea9985f4fb010f6ea676655
Rodrigo Kalil <rkbuarque@gmail.com>
2023-12-01 10:10:46 -0300

    feat: adds on purchases microservice its functionalities

--- purchases/src/http/graphql/resolvers/purchases.resolver.ts
+++ purchases/src/http/graphql/resolvers/purchases.resolver.ts
@@ -1,15 +1,26 @@
 import { UseGuards } from '@nestjs/common';
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { ProductsService } from '../../../services/products.service';
 import { PurchasesService } from '../../../services/purchases.service';
 import { AuthorizationGuard } from '../../../authorization/authorization.guard';
-import { Purchase } from '../models/purchases';
+import { Purchase } from '../models/purchase';
+import { CurrentUser, AuthUser } from '../../../authorization/current-user';
+import { CreatePurchaseInput } from '../inputs/create-purchase-input';
+import { CustomersService } from '../../../services/customers.service';
 
 @Resolver(() => Purchase)
 export class PurchasesResolver {
   constructor(
     private purchasesService: PurchasesService,
     private productsService: ProductsService,
+    private customersService: CustomersService,
   ) {}
 
   @Query(() => [Purchase])
@@ -21,5 +32,27 @@ export class PurchasesResolver {
   @ResolveField()
   product(@Parent() purchase: Purchase) {
     return this.productsService.getProductById(purchase.productId);
+  }
+
+  @Mutation(() => Purchase)
+  @UseGuards(AuthorizationGuard)
+  async createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() user: AuthUser,
+  ) {
+    let customer = await this.customersService.getCustomerByAuthUserId(
+      user.sub,
+    );
+
+    if (!customer) {
+      customer = await this.customersService.createCustomer({
+        authUserId: user.sub,
+      });
+    }
+
+    return this.purchasesService.createPurchase({
+      customerId: customer.id,
+      productId: data.productId,
+    });
   }
 }
--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -4,10 +4,16 @@ import { DatabaseModule } from '../database/database.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
-import { ApolloDriver } from '@nestjs/apollo';
+import {
+  ApolloDriver,
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { ProductsService } from '../services/products.service';
 import { PurchasesService } from '../services/purchases.service';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { CustomersService } from '../services/customers.service';
 
 @Module({
   imports: [
@@ -23,6 +29,8 @@ import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
     ProductsService,
     PurchasesResolver,
     PurchasesService,
+    CustomersResolver,
+    CustomersService,
   ],
 })
 export class HttpModule {}
