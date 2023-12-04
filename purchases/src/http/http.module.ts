b3d3173dd325529559348a77c5994c48ac855575
Rodrigo Kalil <rkbuarque@gmail.com>
2023-12-04 15:53:45 -0300

    feat: integrates purchases with kafka

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../database/database.module';
 import { GraphQLModule } from '@nestjs/graphql';
-import path from 'node:path';
+//import path from 'node:path';
 import { ProductsResolver } from './graphql/resolvers/products.resolver';
 import {
-  ApolloDriver,
   ApolloFederationDriver,
   ApolloFederationDriverConfig,
 } from '@nestjs/apollo';
@@ -14,14 +13,16 @@ import { PurchasesService } from '../services/purchases.service';
 import { PurchasesResolver } from './graphql/resolvers/purchases.resolver';
 import { CustomersResolver } from './graphql/resolvers/customers.resolver';
 import { CustomersService } from '../services/customers.service';
+import { MessagingModule } from '../messaging/messaging.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
-      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+    MessagingModule,
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: { path: 'src/schema.gql', federation: 2 },
     }),
   ],
   providers: [
